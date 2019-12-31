f65d2191dd64da0ec52c7e2f69a5d0fcbe04cc31
n2ptune <n2ptune@outlook.com>
2020-01-01 05:05:10 +0900

    webpack configure for mini-css-extract-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
+const cssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new cssExtractPlugin({
+        filename:'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css'
+      })
+    ]
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src/'))
